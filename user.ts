a4d9ccfe2ac2ac0a93397e76efcef067970886b9
Wiktor <wiktor01@op.pl>
2025-01-08 11:21:26 +0100

    Add new auth types

--- user.ts
+++ user.ts
@@ -1,10 +1,20 @@
-export type User = {
-    email: string;
-    password: string;
-    userName: string;
-}
+export type SignUpBody = {
+  email: string;
+  password: string;
+  userName: string;
+};
 
 export type LoginBody = {
-    email: string;
-    password: string;
-}
+  email: string;
+  password: string;
+  rememberMe: boolean;
+};
+
+export type SignUpReponse = {
+  token: string;
+  message: string;
+};
+
+export type LoginResponse = {
+  token: string;
+};
